501f1e917b09b837758680ba48628d2943956f9c
emost22 <emoney96@naver.com>
2024-01-06 02:31:27 +0900

    Fix: header styles

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import TmingLogo from './img/tming_logo.png'
+
+export {
+  TmingLogo
+}
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,21 @@
+import styles from './header.module.scss'
+import {TmingLogo} from "../../assets";
+
 const Header = () => {
+  const clickLogo = () => {
+    console.log('logo click');
+  }
+
   return (
       <div>
-        header
+        <div className={styles.wrapper}>
+          <img src={TmingLogo} alt="Tming Logo" style={{cursor: 'pointer'}} onClick={clickLogo}/>
+          <div className={styles.navWrapper}>
+            <div className={styles.navItem}>header1</div>
+            <div className={styles.navItem}>header2</div>
+            <div className={styles.navItem}>header3</div>
+          </div>
+        </div>
       </div>
   );
 }
