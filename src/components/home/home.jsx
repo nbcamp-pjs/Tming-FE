544daef34fb194812bebafec7a7d59a7b0fe8451
emost22 <emoney96@naver.com>
2024-01-09 14:46:49 +0900

    img

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,9 +1,18 @@
 import styles from './home.module.scss'
+import {useEffect, useState} from "react";
+import {getImg} from "../../apis/awss3";
 
 const Home = () => {
+  const [imgUrl, setImgUrl] = useState('')
+
+  useEffect(() => {
+    setImgUrl(getImg("test/loopy-goonchim.png"));
+  }, [])
+
   return (
       <div className={styles.wrapper}>
         This is home page<br/>
+        <img src={imgUrl} width='100px'/><br/>
       </div>
   );
 }
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,21 +1,15 @@
 import styles from './login.module.scss';
 import {useRecoilState} from "recoil";
 import {userState} from "../../states";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {loginUser} from '../../apis/user';
-import {getImg} from "../../apis/awss3";
 
 const Login = () => {
   const [user, setUser] = useRecoilState(userState)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const navigate = useNavigate()
-  const [imgUrl, setImgUrl] = useState('')
-
-  useEffect(() => {
-    setImgUrl(getImg("test/loopy-goonchim.png"));
-  }, [])
 
   const login = () => {
     console.log('email = '+ email);
@@ -42,7 +36,6 @@ const Login = () => {
 
   return (
       <div className={styles.wrapper}>
-        <img src={imgUrl} width='100px'/><br/>
         email: <input type="text" onChange={onChangeEmail} value={email}/><br/>
         password: <input type="password" onChange={onChangePassword} value={password}/><br/>
         <button onClick={login}>login</button>
