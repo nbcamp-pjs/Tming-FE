f17ab33a72a85ace8980cfb1d18df97ae0a828a7
emost22 <emoney96@naver.com>
2024-01-16 21:40:33 +0900

    skills

---
+++ src/utils/skills.js
@@ -0,0 +1,66 @@
+const skills = [
+  {
+    "title": "Java",
+    "value": "JAVA"
+  },
+  {
+    "title": "Python",
+    "value": "PYTHON"
+  },
+  {
+    "title": "JavaScript",
+    "value": "JAVASCRIPT"
+  },
+  {
+    "title": "HTML/CSS",
+    "value": "HTML_CSS"
+  },
+  {
+    "title": "Spring Framework",
+    "value": "SPRING"
+  },
+  {
+    "title": "Kotlin",
+    "value": "KOTLIN"
+  },
+  {
+    "title": "Django",
+    "value": "DJANGO"
+  },
+  {
+    "title": "React",
+    "value": "REACT"
+  },
+  {
+    "title": "Angular",
+    "value": "ANGULAR"
+  },
+  {
+    "title": "SQL",
+    "value": "SQL"
+  },
+  {
+    "title": "MongoDB",
+    "value": "MONGODB"
+  },
+  {
+    "title": "Git",
+    "value": "GIT"
+  },
+  {
+    "title": "Docker",
+    "value": "DOCKER"
+  },
+  {
+    "title": "Amazon Web Services",
+    "value": "AWS"
+  },
+  {
+    "title": "Continuous Integration/Continuous Deployment",
+    "value": "CI_CD"
+  },
+]
+
+export {
+    skills
+}
