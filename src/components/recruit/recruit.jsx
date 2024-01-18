cedf106d8c2f8d4e6b8897a8623d475511d27731
emost22 <emoney96@naver.com>
2024-01-18 22:19:19 +0900

    page

--- src/components/recruit/recruit.jsx
+++ src/components/recruit/recruit.jsx
@@ -1,6 +1,23 @@
 import styles from './recruit.module.scss';
+import alertify from "alertifyjs";
+import 'alertifyjs/build/css/alertify.css';
+import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {useRecoilState} from "recoil";
+import {accessTokenState, refreshTokenState} from "../../states";
 
 const Recruit = () => {
+  const navigate = useNavigate()
+  const [accessToken, setAccessToken] = useRecoilState(accessTokenState)
+  const [refreshToken, setRefreshToken] = useRecoilState(refreshTokenState)
+
+  useEffect(() => {
+    if (!accessToken) {
+      alertify.error("로그인 후 이용해주세요.", "1.2");
+      navigate('/');
+    }
+  }, [])
+
   return (
       <div className={styles.wrapper}>
         this is recruit page
